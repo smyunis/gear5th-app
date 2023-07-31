04e325c45f3a82d0e24c3bda067fb6999b516625
smyunis <salmanmohammed176@gmail.com>
2023-08-01 00:27:09 +0300

    Project structure init

---
+++ cmd/api/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 4
+	var j int = 5
+
+	var sum int = i + j
+
+	fmt.Println(sum)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/gear5th/gear5th-api
+
+go 1.20
